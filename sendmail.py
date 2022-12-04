603995426440b9f5f373f736de411912e194e29b
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-12-04 11:21:46 +1100

    wrap send email login in a reusable function

--- sendmail.py
+++ sendmail.py
@@ -4,24 +4,26 @@ import base64
 from email.message import EmailMessage
 
 
-message = EmailMessage()
+def send_email(recipient, subject, message_content):
+	message = EmailMessage()
 
-message.set_content('This is a draft message.')
+	message['To'] = recipient
+	message['From'] = 'UniteMe <admin@uniteme.app>' # Changing the email here doesn't seem to be portrayed on the email that is sent. Need to investigate.
+	message['Subject'] = subject
+	message.set_content(message_content)
 
-message['To'] = 'tscott8520@gmail.com'
-message['From'] = 'admin@uniteme.app' # Changing this doesn't seem to affect the email that is sent. Need to investigate.
-message['Subject'] = 'Automated draft'
+	# encoded message
+	encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-# encoded message
-encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+	create_message = {
+		'raw': encoded_message
+	}
 
-create_message = {
-	'raw': encoded_message
-}
+	send_message = (services.gmail_service.users().messages().send(
+		userId='me', # Indicates the authenticated user. To my understanding, this could be a literal email address if the Service Account has permission to use it, but I can't get it to work..
+		body=create_message
+	).execute())
 
-send_message = (services.gmail_service.users().messages().send(
-	userId='me', # Indicates the authenticated user. To my understanding, this could be a literal email address if the Service Account has permissions, but I can't get it to work..
-	body=create_message
-).execute())
+	print(F'Email message ID: {send_message["id"]}')
 
-print(F'Email message ID: {send_message["id"]}')
+send_email('tscott8520@gmail.com', 'This is the subject', 'This is the email contents')
